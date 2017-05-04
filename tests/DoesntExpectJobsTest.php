0320e45f7662b66157afb796df58d23da0607148
Rizart Dokollari <r.dokollari@gmail.com>
2017-05-05 02:39:45 +0300

    Complete moving to laravel 5.1

---
+++ tests/DoesntExpectJobsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Jobs\SampleJob;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+class DoesntExpectJobsTest extends TestCase
+{
+    use DispatchesJobs;
+
+    /** @test */
+    public function expects_jobs()
+    {
+        $this->expectsJobs(SampleJob::class);
+
+        $this->dispatch(new SampleJob());
+    }
+
+    /** @test */
+    public function does_not_expect_any_job()
+    {
+        $this->doesntExpectJobs();
+
+        $this->dispatch(new SampleJob());
+    }
+
+    private function doesntExpectJobs()
+    {
+        $mock = Mockery::mock('Illuminate\Bus\Dispatcher[dispatch]', [$this->app]);
+
+        $mock->shouldReceive('dispatch')->never();
+
+        $this->app->instance('Illuminate\Contracts\Bus\Dispatcher', $mock);
+
+        return $this;
+    }
+}
